a3ff9b1656cb8f668916b5bd21c875070717e709
hasanga1 <dinithhasangare@gmail.com>
2025-04-05 18:57:51 +0530

    Add endpoint for candidate resume similarity scoring

--- services/talent-agent/service.py
+++ services/talent-agent/service.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, UploadFile, File
+from score import evaluate_candidate
+from fastapi import FastAPI, UploadFile, File, HTTPException, Request
 from kor.extraction import create_extraction_chain
 from pdf_utils import extract_text
 from llm_utils import llm
@@ -26,3 +27,24 @@ async def extract_resumes(files: list[UploadFile] = File(...)):
         os.remove(file_location)
     
     return {"structuredObjects": structuredObjects}
+
+@app.post("/resumes/similarity")
+async def get_scores(request: Request):
+    try:
+        raw_body = await request.body()
+
+        if not raw_body:
+            raise HTTPException(status_code=400, detail="Request body is empty")
+            
+        try:
+            data = await request.json()
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"JSON parsing error: {str(e)}")
+            
+        structured_object = data["structuredObject"]
+        required_skills = data["requiredSkills"]
+        results = evaluate_candidate(structured_object, required_skills)
+        return results
+    except Exception as e:
+        print(f"Error: {type(e).__name__}: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
