9680e450f67206548626222477707794b9788ba6
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2026-03-05 14:20:09 +0530

    fix: handle regions state to fetch regions on failure

--- apps/meet-app/webapp/src/view/dashboard/panel/analytics.tsx
+++ apps/meet-app/webapp/src/view/dashboard/panel/analytics.tsx
@@ -175,7 +175,7 @@ function Analytics() {
     }
   }, [dispatch, startDate, endDate, regionOption]);
   useEffect(() => {
-    if (regions.state === State.idle) {
+    if (regions.state === State.idle || regions.state === State.failed) {
       dispatch(fetchRegions());
     }
   }, [dispatch, regions.state]);
