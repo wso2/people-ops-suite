7c4b9d30071234bf57003b0809eb56abec8eeb3d
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2026-04-23 10:26:35 +0530

    fix : added place holder for event creator alter query default value

--- apps/meet-app/backend/resources/alterations.sql
+++ apps/meet-app/backend/resources/alterations.sql
@@ -16,4 +16,4 @@ ALTER TABLE people_ops_suite.meeting
 
 -- Add Event Creator column to store the event creation calendarId
 ALTER TABLE people_ops_suite.meeting 
-ADD COLUMN `event_creator` VARCHAR(50) NOT NULL DEFAULT '' AFTER `host_unit`; 
+ADD COLUMN `event_creator` VARCHAR(50) NOT NULL DEFAULT '<ORIGINAL CREATOR>' AFTER `host_unit`; 
